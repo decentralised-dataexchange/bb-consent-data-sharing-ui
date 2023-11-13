442f760c34b7180fb1dfcfce09039daa6217bcd6
George J Padayatti <george.padayatti@igrant.io>
2023-11-13 14:35:01 +0530

    Upd: Add margin top for data agreement policy details
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- src/components/dataAgreement/index.jsx
+++ src/components/dataAgreement/index.jsx
@@ -20,103 +20,112 @@ const DataAgreement = ({
   thirdPartyDataSharing,
 }) => {
   return (
-    <Space size={16} direction="vertical">
-      <Row justify={"center"} align={"middle"}>
-        <Col xs={24}>
-          <Space size={16} direction="vertical">
-            <Card bodyStyle={{ padding: "12px", background: "#f7f6f6" }}>
-              <Space
-                direction="vertical"
-                style={{ width: "100%" }}
-                split={
-                  <Divider style={{ margin: 0, backgroundColor: "#dfdfdf" }} />
-                }
-              >
-                <Row>
-                  <Col flex="100px">Version</Col>
-                  <Col flex="auto" className={cx(tw.dataAgreementValue)}>
-                    {version}
-                  </Col>
-                </Row>
-                <Row>
-                  <Col flex="100px">Purpose</Col>
-                  <Col flex="auto" className={cx(tw.dataAgreementValue)}>
-                    {purpose}
-                  </Col>
-                </Row>
-                <Row>
-                  <Col flex="200px">Purpose Description</Col>
-                  <Col flex="auto" className={cx(tw.dataAgreementValue)}>
-                    {purposeDescription}
-                  </Col>
-                </Row>
-                <Row>
-                  <Col flex="200px">Lawful basis of processing</Col>
-                  <Col flex="auto" className={cx(tw.dataAgreementValue)}>
-                    {lawfulBasisOfProcessing}
-                  </Col>
-                </Row>
-              </Space>
-            </Card>
-            <Card bodyStyle={{ padding: "12px", background: "#f7f6f6" }}>
-              <Space
-                direction="vertical"
-                style={{ width: "100%" }}
-                split={
-                  <Divider style={{ margin: 0, backgroundColor: "#dfdfdf" }} />
-                }
-              >
-                <Row>
-                  <Col flex="100px">Policy URL</Col>
-                  <Col flex="auto" className={cx(tw.dataAgreementValue)}>
-                    <Link underline href={policyUrl} target="_blank">
-                      {policyUrl}
-                    </Link>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col flex="200px">Jurisdiction</Col>
-                  <Col flex="auto" className={cx(tw.dataAgreementValue)}>
-                    {jurisdiction}
-                  </Col>
-                </Row>
-                <Row>
-                  <Col flex="200px">Industry scope</Col>
-                  <Col flex="auto" className={cx(tw.dataAgreementValue)}>
-                    {industrySector}
-                  </Col>
-                </Row>
-                <Row>
-                  <Col flex="200px">Storage location</Col>
-                  <Col flex="auto" className={cx(tw.dataAgreementValue)}>
-                    {storageLocation}
-                  </Col>
-                </Row>
-                <Row>
-                  <Col flex="200px">Data retention period in year (s)</Col>
-                  <Col flex="auto" className={cx(tw.dataAgreementValue)}>
-                    {dataRetentionPeriodInYears} years
-                  </Col>
-                </Row>
-
-                <Row>
-                  <Col flex="200px">Geographic restriction</Col>
-                  <Col flex="auto" className={cx(tw.dataAgreementValue)}>
-                    {geographicRestriction}
-                  </Col>
-                </Row>
-                <Row>
-                  <Col flex="200px">Third party data sharing</Col>
-                  <Col flex="auto" className={cx(tw.dataAgreementValue)}>
-                    {thirdPartyDataSharing}
-                  </Col>
-                </Row>
-              </Space>
-            </Card>
+    <Row justify={"center"} align={"middle"}>
+      <Col xs={24}>
+        <Card
+          bodyStyle={{
+            padding: "12px",
+            background: "#f7f6f6",
+            minWidth: "100%",
+          }}
+        >
+          <Space
+            direction="vertical"
+            style={{ minWidth: "100%" }}
+            split={
+              <Divider style={{ margin: 0, backgroundColor: "#dfdfdf" }} />
+            }
+          >
+            <Row>
+              <Col flex="100px">Version</Col>
+              <Col flex="auto" className={cx(tw.dataAgreementValue)}>
+                {version}
+              </Col>
+            </Row>
+            <Row>
+              <Col flex="100px">Purpose</Col>
+              <Col flex="auto" className={cx(tw.dataAgreementValue)}>
+                {purpose}
+              </Col>
+            </Row>
+            <Row>
+              <Col flex="200px">Purpose Description</Col>
+              <Col flex="auto" className={cx(tw.dataAgreementValue)}>
+                {purposeDescription}
+              </Col>
+            </Row>
+            <Row>
+              <Col flex="200px">Lawful basis of processing</Col>
+              <Col flex="auto" className={cx(tw.dataAgreementValue)}>
+                {lawfulBasisOfProcessing}
+              </Col>
+            </Row>
           </Space>
-        </Col>
-      </Row>
-    </Space>
+        </Card>
+        <Card
+          style={{ marginTop: "16px" }}
+          bodyStyle={{
+            padding: "12px",
+            background: "#f7f6f6",
+            minWidth: "100%",
+          }}
+        >
+          <Space
+            direction="vertical"
+            style={{ minWidth: "100%" }}
+            split={
+              <Divider style={{ margin: 0, backgroundColor: "#dfdfdf" }} />
+            }
+          >
+            <Row>
+              <Col flex="100px">Policy URL</Col>
+              <Col flex="auto" className={cx(tw.dataAgreementValue)}>
+                <Link underline href={policyUrl} target="_blank">
+                  {policyUrl}
+                </Link>
+              </Col>
+            </Row>
+            <Row>
+              <Col flex="200px">Jurisdiction</Col>
+              <Col flex="auto" className={cx(tw.dataAgreementValue)}>
+                {jurisdiction}
+              </Col>
+            </Row>
+            <Row>
+              <Col flex="200px">Industry scope</Col>
+              <Col flex="auto" className={cx(tw.dataAgreementValue)}>
+                {industrySector}
+              </Col>
+            </Row>
+            <Row>
+              <Col flex="200px">Storage location</Col>
+              <Col flex="auto" className={cx(tw.dataAgreementValue)}>
+                {storageLocation}
+              </Col>
+            </Row>
+            <Row>
+              <Col flex="200px">Data retention period in year (s)</Col>
+              <Col flex="auto" className={cx(tw.dataAgreementValue)}>
+                {dataRetentionPeriodInYears} years
+              </Col>
+            </Row>
+
+            <Row>
+              <Col flex="200px">Geographic restriction</Col>
+              <Col flex="auto" className={cx(tw.dataAgreementValue)}>
+                {geographicRestriction}
+              </Col>
+            </Row>
+            <Row>
+              <Col flex="200px">Third party data sharing</Col>
+              <Col flex="auto" className={cx(tw.dataAgreementValue)}>
+                {thirdPartyDataSharing}
+              </Col>
+            </Row>
+          </Space>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
