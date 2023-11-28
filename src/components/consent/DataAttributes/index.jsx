5ec707ae025fad4f5a84f6c0edced1f3e19d3a41
George J Padayatti <george.padayatti@igrant.io>
2023-11-28 15:50:40 +0530

    Upd: Add toggle button near data attributes for selective disclosure
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- src/components/consent/DataAttributes/index.jsx
+++ src/components/consent/DataAttributes/index.jsx
@@ -1,27 +1,44 @@
-import { Card, Divider, Space } from "antd";
+import { Card, ConfigProvider, Divider, Space, Switch } from "antd";
 import PropTypes from "prop-types";
 
 // DataAttributes
 const DataAttributes = ({ dataAttributes }) => {
   return (
     <>
-      <Card bodyStyle={{ padding: "12px", background: "#f7f6f6" }}>
-        <Space
-          direction="vertical"
-          style={{ width: "100%" }}
-          split={<Divider style={{ margin: 0, backgroundColor: "#dfdfdf" }} />}
-        >
-          {dataAttributes.map((dataAttribute, i) => {
-            return (
-              <>
-                <p key={i} style={{ margin: 0 }}>
-                  {dataAttribute.name}
-                </p>
-              </>
-            );
-          })}
-        </Space>
-      </Card>
+      <ConfigProvider
+        theme={{
+          token: {
+            // Seed Token
+            colorPrimary: "#71ce74",
+            borderRadius: 2,
+
+            // Alias Token
+            colorBgContainer: "#f6ffed",
+          },
+        }}
+      >
+        <Card bodyStyle={{ padding: "12px", background: "#f7f6f6" }}>
+          <Space
+            direction="vertical"
+            style={{ width: "100%" }}
+            split={
+              <Divider style={{ margin: 0, backgroundColor: "#dfdfdf" }} />
+            }
+          >
+            {dataAttributes.map((dataAttribute, i) => {
+              return (
+                <div
+                  key={i}
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <p style={{ margin: 0 }}>{dataAttribute.name}</p>
+                  <Switch disabled={true} defaultChecked />
+                </div>
+              );
+            })}
+          </Space>
+        </Card>
+      </ConfigProvider>
     </>
   );
 };
